7d3b93ae938ebf979c63a7d4538dae7180af8ab2
Goniix <dragonixgold@gmail.com>
2024-11-18 15:08:46 +0100

    site upgrade

--- script/src.js
+++ script/src.js
@@ -1,9 +1,19 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function setTopnavMode() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
       x.className = "topnav";
     }
-  }
+}
+
+function scrollCarousel(direction) {
+    const wrapper = document.getElementById('project-wrapper');
+    const scrollAmount = wrapper.clientWidth;
+    if (direction === 'left') {
+        wrapper.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    } else {
+        wrapper.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    }
+}
