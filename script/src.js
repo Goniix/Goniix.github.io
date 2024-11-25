1405099234c361c03d0555149822ed47205cd7d5
Gabriel Redouin-Innecco <dragonixgold@gmail.com>
2024-11-25 15:41:10 +0100

    updated scroll

--- script/src.js
+++ script/src.js
@@ -17,3 +17,29 @@ function scrollCarousel(direction) {
         wrapper.scrollBy({ left: scrollAmount, behavior: 'smooth' });
     }
 }
+$(document).ready(function () {
+  $('a[href*="#"]').on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top -55,
+      },
+      500,
+      "swing"
+    );
+  });
+
+  $(window).on("scroll load", function () {
+    $("section").each(function () {
+      let height = $(this).height();
+      let offset = $(this).offset().top - 200;
+      let top = $(window).scrollTop();
+      let id = $(this).attr("id");
+
+      if (top > offset && top < offset + height) {
+        $(".topnav a").removeClass("active");
+        $(".topnav").find(`[href="#${id}"]`).addClass("active");
+      }
+    });
+  });
+});
